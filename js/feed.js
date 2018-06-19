8e7aad0ddbf429c44c7b9ffe1ba2960ff4cc0faa
taufiq30s <m.taufiq30s@hotmail.com>
2018-06-19 14:48:19 +0700

    Revision 2 : New Content (By Ridho), New Slider (By Rian), Added Content to homepage, New Autofill in search bar

---
+++ js/feed.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+});
+
+$(document).ready(function(){
+
+    // bagian slider 
+    var instances=M.Carousel.getInstance($('.carousel').carousel({
+        dist:0,
+        padding:50,
+        duration:200,
+        noWrap:false,
+        // shift:-10,
+    }));
+    var instances2=M.Carousel.getInstance($('.carousel2').carousel({
+        dist:0,
+        padding:50,
+        duration:200,
+        noWrap:false,
+        // shift:-10,
+    }));
+    var instances3=M.Carousel.getInstance($('.carousel3').carousel({
+        dist:0,
+        padding:50,
+        duration:200,
+        noWrap:false,
+        // shift:-10,
+    }));
+    var instances4=M.Carousel.getInstance($('.carousel4').carousel({
+        dist:0,
+        padding:50,
+        duration:200,
+        noWrap:false,
+        // shift:-10,
+    }));
+    var instances5=M.Carousel.getInstance($('.carousel5').carousel({
+        dist:0,
+        padding:50,
+        duration:200,
+        noWrap:false,
+        // shift:-10,
+    }));
+    $('.prev').click(function(){
+        instances.prev(1);
+    })
+    $('.next').click(function(){
+        instances.next(1);
+    })
+    $('.prev_mac').click(function(){
+        instances2.prev(1);
+    })
+    $('.next_mac').click(function(){
+        instances2.next(1);
+    })
+    $('.prev_ipod').click(function(){
+        instances3.prev(1);
+    })
+    $('.next_ipod').click(function(){
+        instances3.next(1);
+    })
+    $('.prev_iphone').click(function(){
+        instances4.prev(1);
+    })
+    $('.next_iphone').click(function(){
+        instances4.next(1);
+    })
+    $('.prev_ipad').click(function(){
+        instances5.prev(1);
+    })
+    $('.next_ipad').click(function(){
+        instances5.next(1);
+    })
+
+    $('.all').click(function(){
+        slide_open('all');
+    })
+    $('.mac').click(function(){
+        slide_open('mac');
+    })
+    $('.ipod').click(function(){
+        slide_open('ipod');
+    })
+    $('.iphone').click(function(){
+        slide_open('iphone');
+    })
+    $('.ipad').click(function(){
+        slide_open('ipad');
+    })
+    var slider=$(".slider");
+    for(i=0;i<slider.length;i++){
+        slider[i].style.display="none";
+    }
+    slider[0].style.display="block";
+    slide_open('all').click();
+    function slide_open(slide){
+        var slider=$(".slider");
+        for(i=0;i<slider.length;i++){
+            slider[i].style.display="none";
+        }
+        var tab=$(".tab");
+        for(i=0;i<tab.length;i++){
+            tab[i].style.background="white"
+            tab[i].style.color="grey";
+            tab[i].style.borderColor="white";
+        }
+        $("#"+slide+"").css({"display":"block"});
+        $("."+slide+"").css({"background":"white","color":"dodgerblue","border-color":"dodgerblue"});
+        $(".next").click();
+        $(".next_mac").click();
+        $(".next_ipod").click();
+        $(".next_iphone").click();
+        $(".next_ipad").click();
+    }
+    // $("#").style.display="block";
+
+
+    // end bagian slider
+});    
--- js/script.js
+++ js/script.js
@@ -3,30 +3,54 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Parallax.init(elems, 0);
 });
 
+$(window).scroll(function(){
+	var scrollTop = $(this).scrollTop();
+	
+	$('.main-product-info').css({
+		opacity: function() {
+			var elementHeight = $(this).height(),
+			opacity = ((1- (scrollTop) / elementHeight) * 1.1) + 0.2 ;
+			return opacity;
+		}
+	})
+});
+
 $(document).ready(function(){
     $('.dropdown-button').dropdown({
         inDuration: 300,
         outDuration: 225,
         hover: true, // Activate on hover
-        belowOrigin: true, // Displays dropdown below the button
+        coverTrigger: false, // Displays dropdown below the button
         alignment: 'left' ,// Displays dropdown with edge aligned to the left of button
         constrainWidth: true, 
     }
   );
+    $('input.search').autocomplete({
+        data: {
+            "Headset" : null,
+            "Wireless Headset" : null,
+			"Wired Headset" : null,
+			"Headphone" : null,
+			"Wireless Headphone" : null,
+			"Wired Headphone" : null,
+			"Keyboard" : null,
+			"Wireless Keyboard" : null,
+			"Wired Keyboard" : null,
+			"Mechanic Keyboard" : null,
+			"Portable Keyboard" : null,
+			"Gaming Mouse" : null,
+			"Wired Mouse" : null,
+			"Wireless Mouse" : null,
+			"Razer Mouse" : null,
+			"Gaming Mousepad" : null,
+			"Gaming Headphone" : null,
+			"Processor" : null,
+			"intel" : null,
+			"Amd" : null,
+			"Intel Core i3" : null,
+			
+        }
+    });
 })
-/*(function($) {
-		$(function() {
-
-$('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    }
-  );
-
-		}); // End Document Ready
-}); // End of jQuery name space*/
 
         
